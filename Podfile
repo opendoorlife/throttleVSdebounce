8b47a378f5c1286f2ca668c79bea7c0eacfcafab
Jeong Yeon Mun <jdusans@gmail.com>
2021-12-27 12:25:25 +0900

    Throttle과 Debounce동작 비교를 위한 버튼 구현

---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'throttleVSdebounce' do
+  pod 'RxSwift'
+  pod 'RxCocoa'
+  pod 'SnapKit'
+  pod 'BonMot'
+end
--- throttleVSdebounce/ViewController.swift
+++ throttleVSdebounce/ViewController.swift
@@ -6,14 +6,92 @@
 //
 
 import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+import BonMot
+
+var disposeBagContext: UInt8 = 0
 
 class ViewController: UIViewController {
 
+  var disposeBag: DisposeBag {
+    if let disposeBag = objc_getAssociatedObject(self, &disposeBagContext) as? DisposeBag {
+      return disposeBag
+    } else {
+      let disposeBag = DisposeBag()
+      objc_setAssociatedObject(self, &disposeBagContext, disposeBag, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+      return disposeBag
+    }
+  }
+  
+  let button: UIButton = {
+    let btn = UIButton()
+    let config = UIImage.SymbolConfiguration(
+        pointSize: 70, weight: .light, scale: .default)
+    let image = UIImage(systemName: "hand.tap", withConfiguration: config)
+    btn.setImage(image?.tintedImage(color: UIColor.black), for: .normal)
+    btn.setImage(image?.tintedImage(color: UIColor.red), for: .highlighted)
+    btn.imageView?.contentMode = .scaleAspectFill
+
+    return btn
+  }()
+  
+  let buttonTitle: UILabel = {
+    let label = UILabel()
+    label.text = "TOUCH"
+    label.font = .systemFont(ofSize: 20, weight: .bold)
+    label.textAlignment = .center
+    label.textColor = .black
+    return label
+  }()
+  
+  let debugLabel: UILabel = {
+    let label = UILabel()
+    label.text = ""
+    label.font = .systemFont(ofSize: 20, weight: .light)
+    label.textAlignment = .center
+    label.textColor = .red
+    return label
+  }()
+  
+  var touchCount = 0
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    view.addSubview(button)
+    button.snp.makeConstraints { (make) in
+      make.height.width.equalTo(100)
+      make.centerY.centerX.equalToSuperview()
+    }
+    
+    view.addSubview(buttonTitle)
+    buttonTitle.snp.makeConstraints { (make) in
+      make.centerX.equalToSuperview()
+      make.top.equalTo(button.snp.bottom).offset(10)
+    }
+    
+    view.addSubview(debugLabel)
+    debugLabel.snp.makeConstraints { (make) in
+      make.centerX.equalToSuperview()
+      make.top.equalTo(buttonTitle.snp.bottom).offset(10)
+    }
+    
+    configureStream()
   }
-
-
+  
+  func configureStream() {
+    button.rx.tap.asDriver()
+      .do(onNext: {
+        self.touchCount += 1
+      })
+//      .throttle(.seconds(3), latest: true)
+//      .throttle(.seconds(3), latest: false)
+      .debounce(.seconds(3))
+      .drive(onNext: { _ in
+        print("***** :: \(self.touchCount) ")
+        self.debugLabel.text! += "→ \(self.touchCount) "
+      }).disposed(by: disposeBag)
+  }
 }
-
